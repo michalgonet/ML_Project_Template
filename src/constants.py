88fb4040007ddce0a283569f751154d07a4b4a83
Michał Gonet <michal.gonet.mail@gmail.com>
2023-10-05 21:03:40 +0200

    Data transformation done

--- src/constants.py
+++ src/constants.py
@@ -1,3 +1,10 @@
 PATH_TO_DATASET: str = 'Data/students_performance_dataset.csv'
 TEST_SIZE: float = 0.2
 SEED: int = 42
+NUMERICAL_FEATURES: list[str] = ["writing_score", "reading_score"]
+CATEGORICAL_FEATURES: list[str] = ["gender",
+                                   "race_ethnicity",
+                                   "parental_level_of_education",
+                                   "lunch",
+                                   "test_preparation_course"]
+TARGET_COLUMN: str = "math_score"
