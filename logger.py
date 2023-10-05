ecbe0e51e502cae7f801d9b9571280ec5698f9de
Michał Gonet <michal.gonet.mail@gmail.com>
2023-10-05 14:47:06 +0200

    Logging defined

---
+++ logger.py
@@ -0,0 +1,15 @@
+import logging
+from datetime import datetime
+from pathlib import Path
+
+LOG_FILE = f'{datetime.now().strftime("%d_%m_%Y_%H_%M_%S")}.log'
+logs_path = Path.cwd() / 'logs'
+logs_path.mkdir(parents=True, exist_ok=True)
+
+LOG_FILE_PATH = logs_path / LOG_FILE
+
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    format="[ %(asctime)s] %(lineno)d %(name)s -%(levelname)s - %(message)s",
+    level=logging.INFO,
+)
